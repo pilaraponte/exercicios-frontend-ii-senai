b5a51dc7dccc380deac81ad45fd25f068b2b85ec
MARIA PILAR APONTE <maria_aponte@estudante.sesisenai.org.br>
2024-09-19 22:06:22 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+  <>
+    <header>Cabeçalho</header>
+    <aside>Lado Esquerdo</aside>
+    <main>Principal</main>
+    <footer>Rodapé</footer>
+  </>
+  );
+}
+
+export default App
