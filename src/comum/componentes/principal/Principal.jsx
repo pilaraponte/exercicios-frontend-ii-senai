9350491b70e333c76fd0daafd46076b5eee852aa
Pilar <mariapilar.aponte@gmail.com>
2024-10-10 23:00:15 -0300

    Aula 10/10 Lista de Produtos

--- src/comum/componentes/principal/Principal.jsx
+++ src/comum/componentes/principal/Principal.jsx
@@ -1,11 +1,17 @@
-
+import { Link } from 'react-router-dom';
 import './Principal.css';
 
-function Principal({children}) {
+function Principal({ voltarPara, titulo, children }) {
   return (
     <main className="principal_root">
-     {children}
-     </main>
+      <div className="principal_titulo">
+        {voltarPara && <Link to={voltarPara}>Voltar</Link>}
+
+        <h1>{titulo}</h1>
+      </div>
+
+      {children}
+    </main>
   );
 }
 
--- src/paginas/PaginaInicial/PaginaInicial.jsx
+++ src/paginas/PaginaInicial/PaginaInicial.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import BotaoCustomizado from '../../comum/componentes/BotaoCustomizado/BotaoCustomizado';
-import Principal from '../../comum/componentes/principal/Principal';
+import Principal from '../../comum/componentes/Principal/Principal';
 import './PaginaInicial.css';
 
 const PaginaInicial = () => {
@@ -10,7 +10,7 @@ const PaginaInicial = () => {
     <Principal titulo="Página Inicial">
       <BotaoCustomizado
         cor="primaria"
-        onClick={() => navigate('/lista-produtos')}
+        aoClicar={() => navigate('/lista-produtos')}
       >
         Lista Produtos
       </BotaoCustomizado>
